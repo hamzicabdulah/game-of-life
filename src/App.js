04c10356ab289e03994ae9724a163e0c618b12a5
hamzicabdulah <hamzic@live.com>
2017-07-02 22:18:45 +0200

    Initial commit — Generating a board with specific number of cells

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import Cell from './Cell';
+
+class App extends Component {
+  state = {
+    cellsArr: generateArray(70, 50)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.cellsArr.map((row) =>
+					<div>
+            {row.map((cell) => 
+              <Cell />
+            )}
+          </div>
+				)}
+      </div>
+    );
+  }
+}
+
+function generateArray(columns, rows) {
+  let cellsArr = [];
+  for (var row = 1; row <= rows; row++) {
+    cellsArr.push([]);
+  }
+  cellsArr.forEach((row) => {
+    for (var col = 1; col <= columns; col++) {
+      row.push(col);
+    }
+  })
+  return cellsArr;
+}
+
+export default App;
