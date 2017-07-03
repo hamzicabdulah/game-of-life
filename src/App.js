c0f84f644151146a9e1655be9379c141e3fcace7
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-07-03 18:25:24 +0000

    Merged table cells

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.cellsArr.map((row) =>
-					<div>
+					<div className="Row">
             {row.map((cell) => 
               <Cell />
             )}
