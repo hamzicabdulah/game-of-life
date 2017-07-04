4972302dd1a4ff086d6ab269685b55d1afc1a6e3
hamzicabdulah <hamzic@live.com>
2017-07-04 20:23:30 +0200

    Added function to change cell state between dead and alive

--- src/Cell.js
+++ src/Cell.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
+  state = {
+    life: "dead"
+  }
+
+  changeLife = () => {
+    //Change state of cell from dead to alive and vice versa
+    this.setState({life: this.state.life === "dead" ? "alive" : "dead"});
+  }
+
   render() {
     return (
-      <div className={"Cell " + this.props.number}>
+      <div className={"Cell " + this.state.life + " " + this.props.number} onClick={this.changeLife}>
       </div>
     );
   }
