80d044084186b797fabf6be56637df2ed082a09e
hamzicabdulah <hamzic@live.com>
2017-07-05 16:06:07 +0200

    Added not fully functional table of Game of Life

--- src/App.js
+++ src/App.js
@@ -2,30 +2,120 @@ import React, { Component } from 'react';
 import './App.css';
 import Cell from './Cell';
 
+let rows = 50;
+let columns = 70;
+
 class App extends Component {
   state = {
-    cellsArr: generateArray(70, 50)
+    cells: generateCells(columns, rows)
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.nextGen, 9000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.cellsArr.map((cell) =>
-          <Cell number={cell}/>
-				)}
+        <div className="container">
+          {Object.keys(this.state.cells).map((number) =>
+            <Cell number={number} life={this.state.cells[number]} changeLife={() => this.changeLife(number)}/>
+          )}
+        </div>
       </div>
     );
   }
+
+  changeLife = (number) => {
+    //Set life of cell with a given number to dead if it is alive, and vice versa
+    let cells = this.state.cells;
+    cells[number] = cells[number] === 'dead' ? 'alive' : 'dead';
+    this.setState({cells: cells});
+  }
+
+  nextGen = () => {
+    //Go through every cell and update their life with "dead" or "alive", depending on their neighbor cells
+    let cells = this.state.cells;
+    Object.keys(cells).forEach((number) => {
+      let life = this.killOrGiveBirth(number);
+      cells[number] = life;
+    });
+    this.setState({cells: cells});
+  }
+  
+  killOrGiveBirth = (number) => {
+    let cellsToCheck = neighborCells(number, columns, rows);
+    let aliveNeighbors = 0;
+
+    cellsToCheck.forEach((cellNum) => {
+      if (this.state.cells[cellNum] === 'alive') aliveNeighbors++;
+    });
+
+    return deadOrAlive(this.state.cells[number], aliveNeighbors);
+  }
+  
 }
 
-function generateArray(columns, rows) {
-  let cellsArr = [];
+function generateCells(columns, rows) {
+  //Generate an object with cell numbers as keys and cell lives/states (dead/alive) as values
+  let cells = {};
   let totalCells = columns * rows;
 
   for (var cell = 1; cell <= totalCells; cell++) {
-    cellsArr.push(cell);
+    //When first generating, assign dead or alive state to a cell randomly
+    cells[cell] = ['alive', 'dead'][Math.floor(Math.random() * 2)];
   }
-  return cellsArr;
+  return cells;
+}
+
+function neighborCells(numberStr, columns, rows) {
+  //Return an array of cell numbers of the neighbors of a cell with a specific number given as argument
+  let neighborsArray = [];
+  let number = +numberStr;
+
+  if(number % columns !== 0) {
+    //If cell has a neighbor on the right, push it to the array
+    neighborsArray.push(number + 1);
+  }
+  if(number !== 1 && (number - 1) % columns !== 0) {
+    //If cell has a neighbor on the left, push it to the array
+    neighborsArray.push(number - 1);
+  }
+  if(number > columns) {
+    //If cell has a neighbor right above it, push it to the array
+    neighborsArray.push(number - columns);
+    if(number % columns !== 0) {
+      //If cell has a neighbor on the right side above it, push it to the array
+      neighborsArray.push(number + 1 - columns);
+    }
+    if(number !== 1 && (number - 1) % columns !== 0) {
+      //If cell has a neighbor on the left side above it, push it to the array
+      neighborsArray.push(number - 1 - columns);
+    }
+  }
+  if(number < columns * rows - columns) {
+    //If cell has a neighbor right below it, push it to the array
+    neighborsArray.push(number + columns);
+    if(number % columns !== 0) {
+      //If cell has a neighbor on the right side below it, push it to the array
+      neighborsArray.push(number + 1 + columns);
+    }
+    if(number !== 1 && (number - 1) % columns !== 0) {
+      //If cell has a neighbor on the left side below it, push it to the array
+      neighborsArray.push(number - 1 + columns);
+    }
+  }
+
+  return neighborsArray;
+}
+
+function deadOrAlive(cellLife, aliveNeighbors) {
+  //Return dead if cell should die, or alive if cell should be born (depending on the aliveNeighbors)
+  return (aliveNeighbors === 3 || (aliveNeighbors === 2 && cellLife === 'alive')) ? 'alive' : 'dead';
 }
 
 export default App;
--- src/Cell.js
+++ src/Cell.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
-  state = {
-    life: "dead"
-  }
-
-  changeLife = () => {
-    //Change state of cell from dead to alive and vice versa
-    this.setState({life: this.state.life === "dead" ? "alive" : "dead"});
-  }
-
   render() {
     return (
-      <div className={"Cell " + this.state.life + " " + this.props.number} onClick={this.changeLife}>
+      <div className={"Cell " + this.props.life + " " + this.props.number} onClick={this.props.changeLife}>
       </div>
-    );
-  }
+    );  }
+
 }
 
 export default Cell;
